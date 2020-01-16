cf651603b4ecf7ac83fb5f89c6cc94f8439f9b0e
SergioCanto <enanifero@hotmail.com>
2020-01-16 08:33:54 -0600

    Examen

---
+++ Parte-3/js/index.js
@@ -0,0 +1,19 @@
+function add(){
+    
+    let btn = document.getElementById('submit');
+
+    btn.addEventListener('click', function(event){
+        event.preventDefault();
+        
+        let name = document.getElementsByClassName('userName');
+        let comment = getElementById('userComment');
+
+        if(name != "" && comment != ""){
+            
+        }
+
+    });
+
+}
+
+add();
